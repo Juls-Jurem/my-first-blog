1ab9d6660e9bbe80e9d74597dc7e0c7c3d983517
Juls-Jurem <juls.jurem@gmail.com>
2025-04-22 11:54:42 +0200

    N

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q5un5j^ja0w3amt&$73@0q+-&nku3)mjocyprk0b8s@8qku0xy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
